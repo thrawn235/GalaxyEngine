3f30e3993506fc5f8b51328b0ba0a31b1dd551fb
Sebastian <thrawn235@gmail.com>
2021-05-03 19:57:18 +0200

    commit

--- source/ObjectsEngine.h
+++ source/ObjectsEngine.h
@@ -32,6 +32,18 @@ class GameEngine;
 #define OBJECTS_TYPE_VECTOR    1
 //====================================
 
+struct Grid
+{
+    unsigned int                id;
+    unsigned int                width;
+    unsigned int                height;
+    unsigned int                tileWidth;
+    unsigned int                tileHeight;
+    unsigned int                offsetX;
+    unsigned int                offsetY;
+    vector<vector<Object*>>     objects;
+};
+
 
 class ObjectsEngine
 {
@@ -71,41 +83,49 @@ public:
 
     //------------------------ purging objects ----------------------------
     //(purge = delete objects immediatly. might lead to broken pointers)
-    virtual     void                PurgeAllObjects             ()                                                                  = 0;
-    virtual     void                PurgeAllObjectsExcept       ( Object* object )                                                  = 0;
-    virtual     void                PurgeAllObjectsExcept       ( vector<Object*> objects )                                         = 0;
-    virtual     void                PurgeAllObjects             ( bool includePersistent )                                          = 0;
-    virtual     void                PurgeAllObjectsExcept       ( Object* object, bool includePersistent )                          = 0;
-    virtual     void                PurgeAllObjectsExcept       ( vector<Object*> objects, bool includePersistent )                 = 0;
-    virtual     void                PurgeObject                 ( unsigned long uid )                                               = 0;
-    virtual     void                PurgeObject                 ( Object* object )                                                  = 0;
-    virtual     void                PurgeObjects                ( vector<unsigned long> uids )                                      = 0;
-    virtual     void                PurgeObjects                ( vector<Object*> objects )                                         = 0;
+    virtual     void                PurgeAllObjects             ()                                                                                                                                          = 0;
+    virtual     void                PurgeAllObjectsExcept       ( Object* object )                                                                                                                          = 0;
+    virtual     void                PurgeAllObjectsExcept       ( vector<Object*> objects )                                                                                                                 = 0;
+    virtual     void                PurgeAllObjects             ( bool includePersistent )                                                                                                                  = 0;
+    virtual     void                PurgeAllObjectsExcept       ( Object* object, bool includePersistent )                                                                                                  = 0;
+    virtual     void                PurgeAllObjectsExcept       ( vector<Object*> objects, bool includePersistent )                                                                                         = 0;
+    virtual     void                PurgeObject                 ( unsigned long uid )                                                                                                                       = 0;
+    virtual     void                PurgeObject                 ( Object* object )                                                                                                                          = 0;
+    virtual     void                PurgeObjects                ( vector<unsigned long> uids )                                                                                                              = 0;
+    virtual     void                PurgeObjects                ( vector<Object*> objects )                                                                                                                 = 0;
 
     //--------------------- store/restore objects -------------------------
     //(store = move to background list to be reused later)
-    // virtual     int              CreateStore                 ()                                                                  = 0;
-    // virtual     void             DeleteStore                 ( int storeID )                                                     = 0;
-    // virtual     void             StoreAllObjects             ()                                                                  = 0;
-    // virtual     void             StoreAllObjectsExcept       ( Object* object, int storeID )                                     = 0;
-    // virtual     void             StoreAllObjectsExcept       ( vector<Object*> objects, int storeID )                            = 0;
-    // virtual     void             StoreAllObjects             ( bool includePersistent, int storeID )                             = 0;
-    // virtual     void             StoreAllObjectsExcept       ( Object* object, bool includePersistent, int storeID )             = 0;
-    // virtual     void             StoreAllObjectsExcept       ( vector<Object*> objects, bool includePersistent, int storeID )    = 0;
-    // virtual     void             StoreObject                 ( unsigned long uid ,int storeID )                                  = 0;
-    // virtual     void             StoreObject                 ( Object* object, int storeID )                                     = 0;
-    // virtual     void             StoreObjects                ( vector<unsigned long> uids, int storeID )                         = 0;
-    // virtual     void             StoreObjects                ( vector<Object*> objects, int storeID )                            = 0;
-    // virtual     void             RestoreAllObjects           ()                                                                  = 0;
-    // virtual     void             RestoreAllObjectsExcept     ( Object* object, int storeID )                                     = 0;
-    // virtual     void             RestoreAllObjectsExcept     ( vector<Object*> objects, int storeID )                            = 0;
-    // virtual     void             RestoreAllObjects           ( bool includePersistent, int storeID )                             = 0;
-    // virtual     void             RestoreAllObjectsExcept     ( Object* object, bool includePersistent, int storeID )             = 0;
-    // virtual     void             RestoreAllObjectsExcept     ( vector<Object*> objects, bool includePersistent, int storeID )    = 0;
-    // virtual     void             RestoreObject               ( unsigned long uid ,int storeID )                                  = 0;
-    // virtual     void             RestoreObject               ( Object* object, int storeID )                                     = 0;
-    // virtual     void             RestoreObjects              ( vector<unsigned long> uids, int storeID )                         = 0;
-    // virtual     void             RestoreObjects              ( vector<Object*> objects, int storeID )                            = 0;
+    // virtual     int              CreateStore                 ()                                                                                                                                          = 0;
+    // virtual     void             DeleteStore                 ( int storeID )                                                                                                                             = 0;
+    // virtual     void             StoreAllObjects             ()                                                                                                                                          = 0;
+    // virtual     void             StoreAllObjectsExcept       ( Object* object, int storeID )                                                                                                             = 0;
+    // virtual     void             StoreAllObjectsExcept       ( vector<Object*> objects, int storeID )                                                                                                    = 0;
+    // virtual     void             StoreAllObjects             ( bool includePersistent, int storeID )                                                                                                     = 0;
+    // virtual     void             StoreAllObjectsExcept       ( Object* object, bool includePersistent, int storeID )                                                                                     = 0;
+    // virtual     void             StoreAllObjectsExcept       ( vector<Object*> objects, bool includePersistent, int storeID )                                                                            = 0;
+    // virtual     void             StoreObject                 ( unsigned long uid ,int storeID )                                                                                                          = 0;
+    // virtual     void             StoreObject                 ( Object* object, int storeID )                                                                                                             = 0;
+    // virtual     void             StoreObjects                ( vector<unsigned long> uids, int storeID )                                                                                                 = 0;
+    // virtual     void             StoreObjects                ( vector<Object*> objects, int storeID )                                                                                                    = 0;
+    // virtual     void             RestoreAllObjects           ()                                                                                                                                          = 0;
+    // virtual     void             RestoreAllObjectsExcept     ( Object* object, int storeID )                                                                                                             = 0;
+    // virtual     void             RestoreAllObjectsExcept     ( vector<Object*> objects, int storeID )                                                                                                    = 0;
+    // virtual     void             RestoreAllObjects           ( bool includePersistent, int storeID )                                                                                                     = 0;
+    // virtual     void             RestoreAllObjectsExcept     ( Object* object, bool includePersistent, int storeID )                                                                                     = 0;
+    // virtual     void             RestoreAllObjectsExcept     ( vector<Object*> objects, bool includePersistent, int storeID )                                                                            = 0;
+    // virtual     void             RestoreObject               ( unsigned long uid ,int storeID )                                                                                                          = 0;
+    // virtual     void             RestoreObject               ( Object* object, int storeID )                                                                                                             = 0;
+    // virtual     void             RestoreObjects              ( vector<unsigned long> uids, int storeID )                                                                                                 = 0;
+    // virtual     void             RestoreObjects              ( vector<Object*> objects, int storeID )                                                                                                    = 0;
+
+    //--------------------------- Grid -----------------------------------
+    virtual     unsigned int        CreateGrid                  ( unsigned int width, unsigned int height, unsigned int tileWidth, unsigned int tileWidth, unsigned int offsetX, unsigned int offsetY )     = 0;
+    virtual     Grid*               GetGrid                     ( unsigned int id )                                                                                                                         = 0;
+    virtual     vector<Grid*>       GetAllGrids                 ()                                                                                                                                          = 0;
+    virtual     void                PopulateGrid                ( unsigned int gridID, unsigned int assetID )                                                                                               = 0;
+    virtual     void                DeleteGrid                  ( unsigned int id )                                                                                                                         = 0;
+    virtual     void                DeleteAllGrids              ()                                                                                                                                          = 0;
 };
 
 #endif
