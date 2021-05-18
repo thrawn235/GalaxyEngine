23c019dff2513ef5838d12f0b9acbb19ad1daef5
Sebastian <thrawn235@gmail.com>
2021-05-18 22:27:33 +0200

    commit

---
+++ assets/maps/basic.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.6.0" name="basic" tilewidth="24" tileheight="24" tilecount="100" columns="10">
+ <image source="../raw/rgb/basicTiles.bmp" width="240" height="240"/>
+</tileset>
